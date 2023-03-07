7cb84f4b5dc482ea520fdc5d156d4a36b3513c0d
GRACIEST <63868635+grishaosher@users.noreply.github.com>
2023-03-07 12:13:09 +0500

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,27 @@
-/* Модальное окно */
+// Модальное окно 
 
 var modal = document.getElementById("my_modal");
 var btn = document.getElementById("btn_modal_window");
+var span = document.getElementsByClassName("close_modal_window")[0];
+
+btn.onclick = function () {
+   modal.style.display = "block";
+}
+
+span.onclick = function () {
+   modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+   if (event.target == modal) {
+       modal.style.display = "none";
+   }
+}
+
+// Второе модальное окно
+
+var modal = document.getElementById("my_modal");
+var btn = document.getElementById("btn_modal_window2");
 var span = document.getElementsByClassName("close_modal_window")[0];
 
 btn.onclick = function () {
